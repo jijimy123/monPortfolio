c6befc6629e1df5ac3a7f559c1d3e544eb09041e
jijimy <jijimy123@gmail.com>
2021-03-04 11:56:54 -0500

    ajouter tailwind

--- src/Composants/Entete.js
+++ src/Composants/Entete.js
@@ -2,10 +2,10 @@ import "./Entete.scss";
 export default function Entete() {
   return (
     <header className="Entete">
-      <ul className="navPrincipale">
-        <li>Projets</li>
-        <li>Contact</li>
-        <li>À props</li>
+      <ul className="navPrincipale flex flex-row justify-between">
+        <li className="bg-green-700 font-bold">Projets</li>
+        <li className="bg-green-700 font-bold">Contact</li>
+        <li className="bg-green-700 font-bold">À props</li>
       </ul>
     </header>
   );
