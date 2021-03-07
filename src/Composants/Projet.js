7b01663aca96ee283bd98775752f124581162c52
jijimy <jijimy123@gmail.com>
2021-03-06 22:30:07 -0500

    enleve un icon

--- src/Composants/Projet.js
+++ src/Composants/Projet.js
@@ -1,4 +1,3 @@
-import { BiLeftArrowCircle } from "react-icons/bi";
 export const Projet = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
